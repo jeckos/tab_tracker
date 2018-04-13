c6c6fc89f9f62299ea085c65affb0a27905f37f8
jeckos <jeckosvoloshyn@gmail.com>
2018-04-13 10:33:22 +0300

    add vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,46 +1,68 @@
 <template>
-  <div>
-    <input
-      type="email"
-      name="email"
-      placeholder="Email"
-      v-model="email"
-    >
-    <br>
-    <input
-      type="password"
-      name="password"
-      placeholder="Password"
-      v-model="password"
-    >
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3 align-center>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="blue mt-5" >
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <div class="error" v-html="error"></div>
+          <v-form v-model="valid">
+            <v-text-field
+              label="Email"
+              v-model="email"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Password"
+              v-model="password"
+              type="password"
+              required
+            ></v-text-field>
+          </v-form>
+          <v-btn
+            class="red"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+
 </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 
 export default {
-  name: "Register",
+  name: 'Register',
   data () {
     return {
+      valid: false,
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const res = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(res.data.message)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
